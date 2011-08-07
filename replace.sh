65f2220bb4d6cc509b499273a509dc2d59dd6aa8
Nicolas Patry <nicolas.patry@student.ecp.fr>
2011-08-07 13:44:07 +0200

    Corrected replace script

--- replace.sh
+++ replace.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+sudo apt-get install ruby-dev rake rubygems pyflakes pep8
+
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
@@ -12,9 +15,12 @@ do
     ln -s $SCRIPTPATH/$file
 done
 
-git submodule --init update
-git submodule foreach git submodule --init update
+cd $SCRIPTPATH
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
 
 cd $HOME/.vim/bundle/command-t/
 rake make
-cd $HOME
+cd $SCRIPTPATH
