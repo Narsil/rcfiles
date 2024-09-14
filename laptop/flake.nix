ee42e606e04657da9570ec407f0e9d2f62fa88e2
Nicolas Patry <patry.nicolas@protonmail.com>
2024-09-15 00:09:17 +0200

    Flake in laptop.

---
+++ laptop/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    kolide-launcher = {
+      url = "github:/kolide/nix-agent/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, kolide-launcher, ... }@inputs: {
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+         # make home-manager as a module of nixos
+         # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+         home-manager.nixosModules.home-manager
+         {
+           home-manager.useGlobalPkgs = true;
+           home-manager.useUserPackages = true;
+           home-manager.users.nicolas = import ../common/home.nix;
+         }
+         kolide-launcher.nixosModules.kolide-launcher
+      ];
+    };
+  };
+}
