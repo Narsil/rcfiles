589911e0c3a96c0167c5ab5e3f81810575023571
Nicolas Patry <patry.nicolas@protonmail.com>
2025-04-10 09:23:00 +0200

    Enabling mosh.

--- common/configuration.nix
+++ common/configuration.nix
@@ -397,6 +397,7 @@
       ];
     };
   };
+  programs.mosh.enable = true;
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
