f70a8b79465970a655a6934df8a86ea614c0034e
Nicolas Patry <patry.nicolas@protonmail.com>
2025-04-16 13:21:35 +0200

    Adding ge-proton.

--- common/configuration.nix
+++ common/configuration.nix
@@ -391,6 +391,7 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
 
   # List services that you want to enable:
